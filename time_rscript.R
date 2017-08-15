7eacdf66a7145848e353c178d5ff7cd2c02bc628
Adam Finnemann <adam.finnemann@gmail.com>
2017-08-15 15:21:56 +0200

    #Changed 3 hours to seconds, add a test time of <1 sec, and added script

--- time_rscript.R
+++ time_rscript.R
@@ -16,6 +16,7 @@ setwd("~/Historie projekt/working_time")
 
 #read file
 
+#first 3 house loaded in seconds
 timer_da <- read.csv("timer_da.csv") %>% 
   mutate(time = as.POSIXct(time),
          status = as.character(status))
@@ -27,6 +28,7 @@ timer_da <- timer_begin(timer_da)
 timer_da <- timer_end(timer_da)
 
 #save file
+
 write.csv(timer_da, "timer_da.csv", row.names = F)
 
 #save to git
